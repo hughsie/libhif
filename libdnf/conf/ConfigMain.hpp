8f6c63f9e7b1e5f6d6785dbd47908fdc2b745228
Evan Goode <mail@evangoo.de>
2024-11-07 02:34:37 +0000

    Add `persistence` config option
    Upstream commit: 18a7db66bfc2cf228bdbb484fa92504c2eaefdc6
    Resolves: https://issues.redhat.com/browse/RHEL-78024

--- libdnf/conf/ConfigMain.hpp
+++ libdnf/conf/ConfigMain.hpp
@@ -123,6 +123,7 @@ public:
     OptionString & comment();
     OptionBool & downloadonly();
     OptionBool & ignorearch();
+    OptionEnum<std::string> & persistence();
 
     OptionString & module_platform_id();
     OptionBool & module_stream_switch();
